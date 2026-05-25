7cb8d2cd63c7dffd08cba8c752cfda93531565b3
HUGOHUGOHUGOHUGOHUGOHUGOHUGOHUGO <hugo.augusto2024@gmail.com>
2026-05-25 20:18:56 -0300

    Update API endpoint to use GEMINI_API_KEY

--- cloudflare-worker.js
+++ cloudflare-worker.js
@@ -36,7 +36,7 @@ export default {
     let geminiRes, data;
     try {
       geminiRes = await fetch(
-        `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${env.sk-ant-api03-AbCdEfGhIjKlMnOpQrStUvWxYz1234567890AbCdEfGhIjKlMnOpQrStUvWxYz1234567890AbCdEfGhIjKlMnOpQrStUv-XXXXXXXX}`,
+        `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${env.GEMINI_API_KEY}`,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
